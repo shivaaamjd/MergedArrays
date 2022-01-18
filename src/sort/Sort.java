2774084a41c59223746c8e251e15452bc9f97b38
shiva <shivaaamjd@gmail.com>
2022-01-18 18:10:51 +0330

    create ArrayLists fill lists from console

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -7,24 +7,39 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Sort {
-    public static void main (String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
 
-       ArrayList <Integer> list1 = new ArrayList<Integer>();
-       ArrayList <Integer> list2 = new ArrayList<Integer>();
-        ArrayList<Integer> list3 = new ArrayList<>();
-        list3.addAll(list1);
-        // for (int i = 0; i < list1.size(); i++) {
+            ArrayList<Integer> list1 = new ArrayList<Integer>();
             list1.addAll(Collections.singleton((in.nextInt())));
+
+            ArrayList<Integer> list2 = new ArrayList<Integer>();
+            list2.addAll(Collections.singleton(in.nextInt()));
+
+            Collections.sort(list1);
+            Collections.sort(list2);
+
             System.out.println(list1);
-      //  }
-        list2.addAll(Collections.singleton(in.nextInt()));
             System.out.println(list2);
-            System.out.println(list3);
+
+            ArrayList<Integer> list3 = new ArrayList<Integer>();
+            // list3.addAll(list1);
+
+          while (in.hasNextInt())
+               {
+
+                 Merge(list1,list2);
+                 System.out.println(Merge(list1,list2));
+               }
         }
+    }
 
-
-       /* private static int Merge (ArrayList a,ArrayList b) {
-            return ArrayList a.add(ArrayList b);
-        }*/
+    private static int Merge(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        while (list1.addAll(list2)) ;
+        Collections.sort(list1);
+        {
+            //System.out.println();
+            return Merge(list1,list2);
+        }
+    }
 }
